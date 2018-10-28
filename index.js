b8daa8227ae25f4fa1f6e0598ac102a743d90293
Aman Mittal <amandeepmittal@live.com>
2018-10-28 16:17:29 +0530

    add babel

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
-
-// Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-	type Query {
-		hello: String
-	}
-`;
-
-// Provide resolver functions for your schema fields
-const resolvers = {
-	Query: {
-		hello: () => 'Hello world!'
-	}
-};
-
-const server = new ApolloServer({ typeDefs, resolvers });
+import express from 'express';
 
 const app = express();
-server.applyMiddleware({ app });
+
+app.get('/', (req, res) => res.send('Babel Working!'));
 
 app.listen({ port: 4000 }, () =>
-	console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+	console.log(`🚀 Server ready at http://localhost:4000`)
 );
